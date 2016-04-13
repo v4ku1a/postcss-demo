ea2bb12cea380eec8d2607aa25bc0a9cc2c5fe3d
v4ku1a <github@vaku.la>
2016-04-13 23:29:18 +0300

    from home
    111

--- gulp-tasks/gulpfile-post.js
+++ gulp-tasks/gulpfile-post.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+
+var processors = [
+	autoprefixer({browsers: ['last 4 versions']})
+];
+
 
 
 gulp.task('post', function () {
     gulp.src('src/*.css')
-        // .pipe()
+        .pipe(postcss(processors))
         .pipe(gulp.dest('build'));
 });
