1c2eef43fce2bc0551f0969c9d37af200516c6e4
Andrii Vakulenko <andrii_vakulemko@epam.com>
2016-04-14 19:24:17 +0300

    From work

--- gulp-tasks/gulpfile-post.js
+++ gulp-tasks/gulpfile-post.js
@@ -2,11 +2,25 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
 var autoprefixer = require('autoprefixer');
+var colorRgbaFallback = require("postcss-color-rgba-fallback");
+
+var atImport = require('postcss-import');
+var mqpacker = require('css-mqpacker');
+var cssnano = require('cssnano');
+
+var nested = require('postcss-nested');
+var fontMagician = require('postcss-font-magician');
+
 
 var processors = [
-	autoprefixer({browsers: ['last 4 versions']})
+	autoprefixer({/*browsers: ['last 2 versions']*/}),
+	colorRgbaFallback,
+	atImport,
+	// cssnano,
+	nested,
+	mqpacker,
+	fontMagician
 ];
-
 
 
 gulp.task('post', function () {
--- gulp-tasks/gulpfile-sass.js
+++ gulp-tasks/gulpfile-sass.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gulpAutoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var combineMq = require('gulp-combine-mq');
+
 
 var sassOptions = {
 	outputStyle: 'expanded'
-}
+};
+var autoPrefOptions = {
+	// browsers: ['last 2 versions']
+};
+
 
 gulp.task('sass', function () {
     gulp.src('src/*.scss')
         .pipe(sass(sassOptions))
+        .pipe(gulpAutoprefixer(autoPrefOptions))
+        // .pipe(cssnano())
+        .pipe(combineMq())
         .pipe(gulp.dest('build'));
 });
