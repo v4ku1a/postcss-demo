6682350818ca19ccade5c63981d1d579f0d9cfef
Andrii Vakulenko <andrii_vakulemko@epam.com>
2016-04-19 19:22:47 +0300

    From work

--- gulp-tasks/gulpfile-post.js
+++ gulp-tasks/gulpfile-post.js
@@ -9,7 +9,10 @@ var mqpacker = require('css-mqpacker');
 var cssnano = require('cssnano');
 
 var nested = require('postcss-nested');
+var cssVariables = require('postcss-css-variables');
 var fontMagician = require('postcss-font-magician');
+
+var bem = require('postcss-bem');
 
 
 var processors = [
@@ -19,12 +22,23 @@ var processors = [
 	// cssnano,
 	nested,
 	mqpacker,
-	fontMagician
+	cssVariables,
+	fontMagician,
+	bem({
+		style:'bem',
+		separators: {
+        	modifier: '--'
+    	},
+		shortcuts: {
+        	component: 'block',
+        	descendent: 'element'
+    	}
+	})
 ];
 
 
 gulp.task('post', function () {
-    gulp.src('src/post.css')
+    gulp.src(['src/post.css', 'src/post-bem.css'])
         .pipe(postcss(processors))
         .pipe(gulp.dest('build'));
 });
